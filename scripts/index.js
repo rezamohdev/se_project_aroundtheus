f2dc7b3822f58f859a2bf84738143631fec072f1
rezamohdev <mohammadireza29@gmail.com>
2023-04-07 14:24:12 -0400

    Javascript add to project

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let initialCards = [
+    {
+        "Yosemite Valley":
+            "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+    },
+
+    {
+        "Lake Louise":
+            "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
+    },
+
+    {
+        "Bald Mountains":
+            "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+    },
+
+    {
+        "Latemar":
+            "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+    },
+    {
+        "Vanoise National Park":
+            "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+    },
+
+    {
+        "Lago di Braies":
+            "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
+    }
+];
